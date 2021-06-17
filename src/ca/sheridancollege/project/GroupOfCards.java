68277bce1f26c1c390a5509fb6ff4f176ce1a47d
Maryam <hisamm@sheridancollege.ca>
2021-06-17 10:06:42 -0400

    Third Commit

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -13,6 +13,7 @@ import java.util.Collections;
  * HINT, you might want to subclass this more than once.
  * The group of cards has a maximum size attribute which is flexible for reuse.
  * @author dancye
+ * @modifier: Maryam Hisam
  */
 public class GroupOfCards 
 {
