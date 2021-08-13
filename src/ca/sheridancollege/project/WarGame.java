200e21cc2c28260f68d41738afb76cd3ad1743e1
evachu86 <evachu86@users.noreply.github.com>
2021-08-14 03:23:24 +0800

    Add WarGame, Modify WarPlayer

---
+++ src/ca/sheridancollege/project/WarGame.java
@@ -0,0 +1,53 @@
+package ca.sheridancollege.project;
+
+public class WarGame extends Game {
+
+	public WarGame(String givenName) {
+		super(givenName);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static int WAR_CARD_NUMBER = 3;
+
+	/**
+	 * Play the game. This might be one method or many method calls depending on your game.
+	 */
+	public void play() {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * When the game is over, use this method to declare and display a winning player.
+	 */
+	public void declareWinner() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void distributeCards() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void guide() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void endEarly() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void abort() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void restart() {
+		throw new UnsupportedOperationException();
+	}
+
+	private int compareCard() {
+		throw new UnsupportedOperationException();
+	}
+
+	private void receiveCards(int playerIndex) {
+		throw new UnsupportedOperationException();
+	}
+}
